b8ab44a551223ad36df8fffcd547596752d238d3
amozhet <220424@astanait.edu.kz>
2024-02-19 21:04:43 +0600

    Поменяла CreateMovieFrom. И Insert из news.go соответсвенно. Добавила env. Добавила таблицу.

---
+++ cmd/web/routes.go
@@ -0,0 +1,40 @@
+//routes.go
+
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf, app.authenticate)
+
+	mux := pat.New()
+
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+	mux.Get("/category/student", dynamicMiddleware.ThenFunc(app.category))
+	mux.Get("/category/staff", dynamicMiddleware.ThenFunc(app.category))
+	mux.Get("/category/applicant", dynamicMiddleware.ThenFunc(app.category))
+	mux.Get("/category/research", dynamicMiddleware.ThenFunc(app.category))
+
+	mux.Get("/movies/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createMoviesForm))
+	mux.Post("/movies/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createMovies))
+
+	mux.Get("/movies/:id", dynamicMiddleware.ThenFunc(app.showMovies))
+
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
+	mux.Get("/user/profile", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.userProfile))
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+
+	return standardMiddleware.Then(mux)
+}
